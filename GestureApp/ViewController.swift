55217df163ae1c8e6303f8ed6543a0af364d7205
fuatbolat <byfuatbolat@gmail.com>
2023-12-12 21:45:12 +0300

    GestureRecognizerApp
    Created an app using GestureRecognizer

--- GestureApp/ViewController.swift
+++ GestureApp/ViewController.swift
@@ -8,11 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var myLabel: UILabel!
+    
+    var isIcardi = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        imageView.isUserInteractionEnabled = true
+        
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTiklandi))        // add this to image view
+        imageView.addGestureRecognizer(gestureRecognizer)
+        
     }
+    @objc func imageTiklandi(){
+        if isIcardi == true {
+            imageView.image = UIImage(named: "muslera")
+            myLabel.text = "son anda "
+        }
+        
+    }
+    
+    
+    
+    
 
 
 }
